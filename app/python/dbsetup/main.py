2bebcbc93d72000e6b0beaa84462046e4bb6bd2d
katjes <katjes733@gmx.net>
2025-03-24 13:03:11 -0700

    feat: TF support and fieldmapping

--- app/python/dbsetup/main.py
+++ app/python/dbsetup/main.py
@@ -26,20 +26,38 @@ master_sql_templates = [
 ]
 
 table_sql_templates = [
-    {"Template": Template("CREATE TABLE IF NOT EXISTS $schema.$table (id uuid PRIMARY KEY, embedding vector($embedding_size), chunks text, metadata json);"), "FailOnError": True},
+    {"Template": Template("CREATE TABLE IF NOT EXISTS $schema.$table ($primary_key_field uuid PRIMARY KEY, $vector_field vector($embedding_size), $text_field text, $metadata_field json);"), "FailOnError": True},
     {"Template": Template("CREATE INDEX IF NOT EXISTS idx_$table ON $schema.$table USING hnsw (embedding vector_cosine_ops) WITH (ef_construction=256);"), "FailOnError": True}
 ]
 
 
+def get_field_mapping(rp):
+    if "FieldMapping" not in rp:
+        raise KeyError("FieldMapping not found in ResourceProperties")
+    if isinstance(rp['FieldMapping'], str):
+        field_mapping = json.loads(rp['FieldMapping'])
+    elif isinstance(rp['FieldMapping'], dict):
+        field_mapping = rp['FieldMapping']
+    else:
+        raise TypeError(f"FieldMapping must be a string or a dictionary, but is {type(rp['FieldMapping'])}")
+
+    if not field_mapping["primary_key_field"]:
+        raise ValueError("primary_key_field cannot be empty")
+
+    return field_mapping
+
+
 def lambda_handler(event, context):
     try:
-        if event['RequestType'] == 'Create':
+        if ('RequestType' in event and event['RequestType'] == 'Create') or \
+                ('tf' in event and 'action' in event['tf'] and event['tf']['action'] == 'create'):
             rp = event['ResourceProperties']
             db_cluster_arn = rp['DbClusterArn']
             master_secret_arn = rp['MasterSecretArn']
             bedrock_secret_arn = rp['BedrockSecretArn']
             database_name = rp['DatabaseName']
             kb_tables = rp['KnowledgeBaseTables']
+            field_mapping = get_field_mapping(rp)
 
             response = secrets_client.get_secret_value(
                 SecretId=bedrock_secret_arn)
@@ -52,6 +70,9 @@ def lambda_handler(event, context):
                 "embedding_size": secret.get('embedding_size')
             }
 
+            for key, value in field_mapping.items():
+                data[key] = value
+
             for sql_template in master_sql_templates:
                 execute_sql(sql_template, data, db_cluster_arn, master_secret_arn, database_name)
 
@@ -61,9 +82,17 @@ def lambda_handler(event, context):
                     execute_sql(sql_template, data, db_cluster_arn, bedrock_secret_arn, database_name)
 
         send_response_cfn(event, context, cfnresponse.SUCCESS)
+        return {
+            "message": "Function executed successfully!"
+        }
     except Exception as e:
         logger.error("Exception: %s", e)
         send_response_cfn(event, context, cfnresponse.FAILED)
+        return {
+            "message": "Error executing function",
+            "error": str(e),
+            "code": "INTERNAL_ERROR"
+        }
 
 
 def execute_sql(
@@ -89,6 +118,8 @@ def execute_sql(
 
 
 def send_response_cfn(event, context, response_status):
+    if 'ResponseURL' not in event:
+        return
     response_data = {}
     response_data['Data'] = {}
     cfnresponse.send(event, context, response_status, response_data, "CustomResourcePhysicalID")
